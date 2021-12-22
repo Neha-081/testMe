35a3b920d3cd8496b322d020fd1cd4497bdfc771
Neha <nehaprajapati1150@gmail.com>
2021-12-22 23:52:05 +0530

    breadth first search

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+const Runner=require('./runner')
+const runner=new Runner();
 
-console.log('running');
+const run= async()=>{
+    const results = await runner.collectFiles(process.cwd());
+    console.log(results);
+}
+run();
