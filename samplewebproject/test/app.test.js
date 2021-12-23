1cd8aa61376ac0828db8ca3b657109b0e0808e78
Neha <nehaprajapati1150@gmail.com>
2021-12-23 13:52:38 +0530

    more on mocha

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,4 +1,5 @@
 const assert=require('assert');
+const render = require('../../render');
 it('has a text input',async()=>{
   const dom=await render('index.html');
 const input=dom.window.document.querySelector('input');
@@ -6,3 +7,16 @@ const input=dom.window.document.querySelector('input');
 assert(input)
 
 });
+
+it('it shows a success message with valid email',async()=>{
+    const dom=await render('index.html');
+    const input=dom.window.document.querySelector('input');
+    input.value='maya@gmail.com'
+    dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+    const h1=dom.window.document.querySelector('h1');
+    console.log('content',h1.innerHTML);
+
+})
